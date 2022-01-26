3984cceff4e120cadb81cd01663821d17185ebea
Sideways-Reptile <impressive-tie@pm.me>
2022-01-26 11:43:18 -0800

    small changes

--- #GitDemo.py
+++ #GitDemo.py
@@ -3,3 +3,8 @@
 #Made a git account; downloaded VSCode and Python to Laptop to use and follow along. 
 #Going to write some code in 100 days of Pythin and push to my GitHub account
 #This is going to be fun
+
+#GODDAMN that was annoying. Hahah. Just have to make sure the right steps are followed. Git -bash now active in Terminal.
+##Moving on##
+#Pulled down the Repo from git using 'git clone [URL]'. can check status of things with 'git status'
+#need to add origin and master to push command
