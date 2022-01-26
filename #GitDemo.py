816fff083a959a4ac66c677fcd612332b9c2b6f1
Sideways-Reptile <impressive-tie@pm.me>
2022-01-26 11:32:37 -0800

    Updated DemoRepo

---
+++ #GitDemo.py
@@ -0,0 +1,5 @@
+#GitDemo
+
+#Made a git account; downloaded VSCode and Python to Laptop to use and follow along. 
+#Going to write some code in 100 days of Pythin and push to my GitHub account
+#This is going to be fun
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This Repo is meant to help me learn how to use Git and github. Following along with FreeCodeAcadmey video.
 
 Learing new things
+
+So far, weve done some things. Lets check status
